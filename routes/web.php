a353fb68fc823c132a5e03bc57bffda47d5c8355
Domi <vataidomonkos@gmail.com>
2024-04-21 20:53:01 +0200

    create housing routes for controller actions

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HousingController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/housings', [HousingController::class, 'list'])->name('housings.list');
+    Route::get('/housings/{id}', [HousingController::class, 'show'])->name('housings.show');
+    Route::patch('/housings/{id}', [HousingController::class, 'update'])->name('housings.update');
+    Route::delete('/housings/{id}', [HousingController::class, 'destroy'])->name('housings.destroy');
+    Route::post('/housings', [HousingController::class, 'store'])->name('housings.store');
 });
 
 require __DIR__.'/auth.php';
