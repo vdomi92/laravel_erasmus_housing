49d42c7acdf37f2eb3349322737019659e132dad
Domi <vataidomonkos@gmail.com>
2024-04-21 20:53:38 +0200

    update navbar with buttons

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,10 +11,8 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
+                <div class="d-flex align-items-center ml-3">
+                    <x-nav-items></x-nav-items>
                 </div>
             </div>
 
