4ff0d6b9c47e9fd8ef033eb812aae2f67d53f7c5
Domi <vataidomonkos@gmail.com>
2024-04-21 20:53:30 +0200

    update navbar with buttons

---
+++ resources/views/components/nav-items.blade.php
@@ -0,0 +1,26 @@
+<div class="d-flex">
+    <div class="dropdown me-0 me-lg-2">
+        <button class="btn btn-secondary  dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Housings
+        </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="{{ route('housings.list') }}">Browse listings</a></li>
+            <li><a class="dropdown-item" href="#">Create listing</a></li>
+            <li><a class="dropdown-item" href="#">My listings</a></li>
+        </ul>
+    </div>
+    <div class="dropdown me-0 me-lg-2">
+        <button class="btn btn-secondary  dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Applications
+        </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">My applications</a></li>
+            <li><a class="dropdown-item" href="#">Applied to me</a></li>
+        </ul>
+    </div>
+    <div class="me-0 me-lg-2">
+        <button class="btn btn-secondary" type="button" >
+            Messages
+        </button>
+    </div>
+</div>
