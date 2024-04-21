d342baaa6590aa297e592348b67b3c047fa6d938
Domi <vataidomonkos@gmail.com>
2024-04-21 17:39:33 +0200

    phpdoc to help ide

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,11 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Builder;
 
+/**
+ * @mixin Builder
+ */
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
