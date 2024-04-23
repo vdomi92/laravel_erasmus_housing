4732c4bab0f601e495f68170dfac4409d96d201e
vdomi92 <vataidomonkos@gmail.com>
2024-04-23 16:40:18 +0200

    image model definition

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -2,10 +2,37 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 
+/**
+ * @mixin Builder
+ */
 class Image extends Model
 {
-    use HasFactory;
+    public $preventsLazyLoading = true;
+
+    protected $fillable = [
+        'is_preview',
+        'filename',
+        'path',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function housing(): BelongsTo
+    {
+        return $this->belongsTo(Housing::class);
+    }
+
+    /**
+     * @return HasOneThrough
+     */
+    public function uploader(): HasOneThrough
+    {
+        return $this->hasOneThrough(User::class, Housing::class);
+    }
 }
