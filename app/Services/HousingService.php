bbceb46f82558ea25dc97a2a426b2ccacfe92605
vdomi92 <vataidomonkos@gmail.com>
2024-04-23 14:10:52 +0200

    getbyid with relations

--- app/Services/HousingService.php
+++ app/Services/HousingService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\Housing;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class HousingService
 {
@@ -15,10 +16,24 @@ class HousingService
      */
     public function list(): Collection
     {
-        return Housing::withCount(['applications as accepted_count' => function ($query) {
-            $query->where('is_accepted', 1);
-        }])->where('accepted_count', '>', 0)
+        return Housing::withAcceptedApplicationsCount()
+            ->where('accepted_count', '>', 0)
             ->groupBy('country', 'city')
             ->get();
+    }
+
+    /**
+     * Finds requested entity by ID or throw ModelNotFoundException<Housing> if not found.
+     *
+     * @param int $id
+     * @return Housing
+     */
+    public function getByIdWithRelations(int $id): Housing
+    {
+        return Housing::with('owner')
+            ->withAcceptedApplicationsCount()
+            ->find($id)
+
+            ?? throw new ModelNotFoundException(Housing::class);
     }
 }
