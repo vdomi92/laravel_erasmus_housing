74626c2f651951740022e23f60597ac83abe52d5
Domi <vataidomonkos@gmail.com>
2024-04-23 21:34:16 +0200

    is_accepted default value update in applications

--- database/migrations/2024_04_19_193139_create_applications_table.php
+++ database/migrations/2024_04_19_193139_create_applications_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->text('message');
             $table->date('start_date');
             $table->unsignedTinyInteger('duration');
-            $table->boolean('is_accepted')->nullable();
+            $table->boolean('is_accepted')->default(null)->nullable();
 
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
