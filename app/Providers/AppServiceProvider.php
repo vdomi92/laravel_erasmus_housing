fa7d16bbd33999f91d5b0eb37613efaa23b1157a
Domi <vataidomonkos@gmail.com>
2024-04-23 21:34:54 +0200

    registered applicationpolicy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Application;
+use App\Policies\ApplicationPolicy;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        Gate::policy(Application::class, ApplicationPolicy::class);
     }
 
     /**
