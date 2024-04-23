1cfa042c5a9cb1f79981dfbe3de095048220cffe
vdomi92 <vataidomonkos@gmail.com>
2024-04-23 14:09:38 +0200

    card update

--- resources/views/components/single-housing-display.blade.php
+++ resources/views/components/single-housing-display.blade.php
@@ -1,4 +1,4 @@
-@props(['housing' => \App\Models\Housing::class])
+@props(['housing'])
 
 <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-4 p-1">
     <div class="card">
@@ -8,11 +8,11 @@
                 <h5 class="fw-bold ">{{ $housing->city }} - {{ $housing->country }}</h5>
             </div>
             <div class="pl-2 pt-2 mb-1">
-                <p class="mb-1">Available places: {{ $housing->nr_of_slots }}</p>
+                <p class="mb-1">Available places: {{ $housing->accepted_count }}</p>
                 <p>{{strlen($housing->description) > 80 ? substr($housing->description, 0,80) . '...' : $housing->description}}</p>
             </div>
             <div class="pb-1 d-flex justify-center mt-auto">
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="{{route('housings.show', ['id' => $housing->id])}}" class="btn btn-primary">View details</a>
             </div>
         </div>
     </div>
