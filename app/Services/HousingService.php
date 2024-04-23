f19445ea715ba9ae8f17b2691757f63b3ac1a179
vdomi92 <vataidomonkos@gmail.com>
2024-04-23 10:40:51 +0200

    service list query updated, still missing pagination

--- app/Services/HousingService.php
+++ app/Services/HousingService.php
@@ -7,11 +7,18 @@ use Illuminate\Database\Eloquent\Collection;
 
 class HousingService
 {
+
     /**
+     * Lists all housings with available places to apply for. Groups by country first and then by city.
+     *
      * @return Collection
      */
     public function list(): Collection
     {
-        return Housing::all();
+        return Housing::withCount(['applications as accepted_count' => function ($query) {
+            $query->where('is_accepted', 1);
+        }])->where('accepted_count', '>', 0)
+            ->groupBy('country', 'city')
+            ->get();
     }
 }
