c0f6032d9950500f5bff47e488c398bf6384910e
Domi <vataidomonkos@gmail.com>
2024-04-24 17:34:07 +0200

    available slot calculations fix

--- resources/views/components/single-housing-display-listed.blade.php
+++ resources/views/components/single-housing-display-listed.blade.php
@@ -2,13 +2,17 @@
 
 <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-4 p-1">
     <div class="card h-100">
-        <img src="..." class="card-img-top col-md-6 img-thumbnail ratio ratio-16x9" alt="TODO">
+        <img
+            src="{{'...'}}"
+            class="card-img-top col-md-6 img-thumbnail ratio ratio-16x9"
+            alt="Preview image">
+
         <div class="d-flex flex-col h-100 justify-content-between">
             <div class="pl-2 pt-3 mb-2">
                 <h5 class="fw-bold ">{{ $housing->city }} - {{ $housing->country }}</h5>
             </div>
             <div class="pl-2 pt-2 mb-1">
-                <p class="mb-1">Available places: {{ $housing->accepted_count }}</p>
+                <p class="mb-1">Available places: {{ $housing->nr_of_slots - $housing->accepted_count }}</p>
                 <p>{{strlen($housing->description) > 80 ? substr($housing->description, 0,80) . '...' : $housing->description}}</p>
             </div>
             <div class="pb-md-1 pb-0 d-flex justify-center mt-auto">
